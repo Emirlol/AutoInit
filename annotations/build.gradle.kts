24d179a0d6aaf0d458d4e9c35889e064f9e05df7
Rime <81419447+Emirlol@users.noreply.github.com>
2025-02-18 20:43:52 +0300

    Add publishing for the annotation

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,8 +1,30 @@
+import magik.createGithubPublication
+import magik.github
+
 plugins {
 	alias(libs.plugins.kotlin)
 	alias(libs.plugins.magik)
+	`maven-publish`
 }
 
 repositories {
 	mavenCentral()
+}
+
+publishing {
+	publications {
+		createGithubPublication {
+			groupId = "me.rime.symbols"
+			artifactId = "init-annotation"
+			version = libs.versions.version.get()
+
+			from(components["java"])
+		}
+	}
+	repositories {
+		github {
+			name = "RimeMaven"
+			domain = "Emirlol/Maven"
+		}
+	}
 }
