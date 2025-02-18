d5233ca9964c1d808f41eea7baff476f31124660
Rime <81419447+Emirlol@users.noreply.github.com>
2025-02-18 21:28:40 +0300

    Try being more explicit with jvm version in hopes of it working

--- symbols/build.gradle.kts
+++ symbols/build.gradle.kts
@@ -1,5 +1,8 @@
+
 import magik.createGithubPublication
 import magik.github
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	alias(libs.plugins.kotlin)
@@ -36,13 +39,25 @@ publishing {
 	}
 }
 
-kotlin {
-	compilerOptions {
-		jvmToolchain(21)
-	}
+val targetJavaVersion = 21
+java {
+	toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+	withSourcesJar()
 }
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_21
-	targetCompatibility = JavaVersion.VERSION_21
+tasks {
+	withType<JavaCompile>().configureEach {
+		// ensure that the encoding is set to UTF-8, no matter what the system default is
+		// this fixes some edge cases with special characters not displaying correctly
+		// see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
+		// If Javadoc is generated, this must be specified in that task too.
+		options.encoding = "UTF-8"
+		options.release.set(targetJavaVersion)
+	}
+
+	withType<KotlinCompile>().configureEach {
+		compilerOptions {
+			jvmTarget.set(JvmTarget.fromTarget(targetJavaVersion.toString()))
+		}
+	}
 }
